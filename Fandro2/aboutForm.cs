b69670b66ef3965d461c8ec810171bb9c8e78b6d
arthur <arthur.hoogervorst>
2023-03-23 21:54:11 -0300

    + added proper versioning information

--- Fandro2/aboutForm.cs
+++ Fandro2/aboutForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fandro2.lib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,12 @@ namespace Fandro2 {
     public partial class aboutForm : Form {
         public aboutForm() {
             InitializeComponent();
+        }
+
+        private void aboutForm_Load(object sender, EventArgs e) {
+            // get the file version
+            this.label3.Text = String.Format("Version: {0}", Helpers.GetFileVersionInfo().FileVersion);
+            this.label2.Text = String.Format("Copyrights © 2005-{0} Arthur Hoogervorst for PPF", DateTime.Now.Year);
         }
     }
 }
